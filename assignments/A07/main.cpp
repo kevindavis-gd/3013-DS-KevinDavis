0c0c5716b53c0b636cbc791006169dd5fb80cfe4
Kevin Davis <45161204+kevindavis-gd@users.noreply.github.com>
2019-12-01 19:51:13 -0600

    Update main.cpp

--- assignments/A07/main.cpp
+++ assignments/A07/main.cpp
@@ -1,1 +1,11 @@
+#include "TermColor.h" // color header file
+#include <fstream>     // file output
+#include <iostream>    // standard output
+#include <map>         // stl map
+#include <math.h>
+#include <stdlib.h> // used for windows system("cls"); // clear screen
+#include <string>
+#include <unistd.h> // used for Griffins macbook clear screen
+#include <vector>   // stl vector
 
+using namespace std;
